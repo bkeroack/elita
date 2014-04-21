bd708ddeb8dd9b746651bb9d02b9ba750d3a3f6e
Ben Keroack <ben@scorebig.com>
2014-04-21 09:59:41 -0700

    fix install path

--- elita_install/linux.py
+++ elita_install/linux.py
@@ -5,12 +5,14 @@ import os
 import os.path
 import sys
 from sh import service
+import logging
 from clint.textui import progress, puts, indent, colored
 
 def get_root_dir():
     return os.path.abspath(os.path.dirname(__file__))
 
 def cp_file_checkperms(src, dest):
+    logging.debug("copying {} to {}".format(src, dest))
     try:
         shutil.copyfile(src, dest)
     except IOError:
@@ -26,7 +28,7 @@ def mk_etc_dir_posix():
             sys.exit(1)
 
 def cp_prod_ini_posix():
-    ini_location = os.path.join(get_root_dir(), "elita.ini")
+    ini_location = os.path.join(get_root_dir(), "util/elita.ini")
     cp_file_checkperms(ini_location, '/etc/elita/elita.ini')
 
 def do_step(msg, func, params=[]):
