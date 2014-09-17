436611037f5a329ab4fbb33c726bfe4344391125
Ben Keroack <ben@scorebig.com>
2014-09-17 13:40:53 -0700

    release

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ requires = [
 ]
 
 setup(name='elita',
-      version="0.60.1",
+      version="0.60.2",
       description='Continuous deployment (continuous delivery) and infrastructure management REST framework',
       long_description=README + '\n\n' + CHANGES,
       license='Apache',
